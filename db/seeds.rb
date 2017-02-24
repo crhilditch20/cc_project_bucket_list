bddcfef253f482a07004cdbccd4ec7087f5d306d
crhilditch20 <crhilditch20@gmail.com>
2017-02-24 15:24:56 +0000

    added seed data and tested in rails console

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,50 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Country.delete_all()
+Experience.delete_all()
+Event.delete_all()
+BucketListCountry.delete_all()
+BucketListExperience.delete_all()
+BucketListEvent.delete_all()
+
+country1 = Country.create({ 
+  name: "Japan",
+  region: "Asia",
+  season: "autumn",
+  visitLength: 2,
+  mapURL: "",
+  imageURL: ""
+})
+country2 = Country.create({ 
+  name: "Cuba",
+  region: "Central America",
+  season: "any",
+  visitLength: 2,
+  mapURL: "",
+  imageURL: ""
+})
+
+experience1 = Experience.create({
+  title: "Scuba diving",
+  description: "Um...scuba diving",
+  season: "summer"
+  })
+experience2 = Experience.create({
+  title: "Kodo drumming",
+  description: "Watch a kodo drumming show",
+  season: "any"
+  })
+
+user1 = User.create({
+  email: "claire@myemail.com",
+  password: "password",
+  password_confirmation: "password"
+  })
+
+listCountry1 = BucketListCountry.create({
+  country: country1,
+  user: user1
+  })
+
+listExperience1 = BucketListExperience.create({
+  experience: experience1,
+  user: user1
+  })
