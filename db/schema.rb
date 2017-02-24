ccf55776533a9642254a030314efa5d2f86dc091
crhilditch20 <crhilditch20@gmail.com>
2017-02-24 15:27:51 +0000

    add missing column to Event model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170224150321) do
+ActiveRecord::Schema.define(version: 20170224152728) do
 
   create_table "bucket_list_countries", force: :cascade do |t|
     t.integer  "country_id"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20170224150321) do
     t.string   "mapURL"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.datetime "date"
   end
 
   create_table "experiences", force: :cascade do |t|
